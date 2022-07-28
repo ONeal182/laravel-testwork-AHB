2d96a53bbb540d4418399555b53e37cd1a12299d
ONeal <kigat63@gmail.com>
2022-07-28 12:52:35 +0400

    add master layout

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="ru">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Тестовая работа AHB</title>
+
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/css/style-template.css" rel="stylesheet">
+    <script src="{{ asset('js/app.js') }}" defer></script>
+</head>
+
+<body>
+
+    <div class="container">
+        @yield('content')
+    </div>
+</body>
