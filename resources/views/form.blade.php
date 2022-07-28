01b497dd929c4b6d150b3759ffcdc083fea0e0a5
ONeal <kigat63@gmail.com>
2022-07-28 14:00:35 +0400

    add btn to form

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -6,5 +6,6 @@
       <label for="exampleFormControlFile1">Example file input</label>
       <input type="file" class="form-control-file" id="exampleFormControlFile1">
     </div>
+    <button type="submit" class="btn btn-primary">Отправить</button>
   </form>
 @endsection
