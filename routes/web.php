95bab59d15b64f330267826b01e64dd45132dd35
ONeal <kigat63@gmail.com>
2022-07-28 13:52:20 +0400

    add controller and route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FormController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,6 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
+Route::get('/form', [App\Http\Controllers\FormController::class, 'index'])->name('form');
 
 Auth::routes();
 
