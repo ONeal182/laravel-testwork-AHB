e2ffbab2c82a6563dcdfc8768c972354426de7e2
ONeal <kigat63@gmail.com>
2022-07-30 09:40:19 +0400

    fix logout

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\Docs\DocsController;
 use App\Http\Controllers\Admin\Product\ProductController;
@@ -32,4 +33,4 @@ Route::middleware(['auth'])->group(function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::get('/logout', 'Auth\LoginController@logout')->name('get-logout');
+Route::get('/logout', [App\Http\Controllers\Auth\LoginController::class, 'logout'])->name('get-logout');
